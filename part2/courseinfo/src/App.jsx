1327f3d404c163a60618f54b9f4ee3dbfd20944d
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-04 13:04:22 -0700

    exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => {
+  return(
+  <div>
+    {props.parts.map(part => {return <Part key={part.id} part={part}/>})}
+  </div>)
+}
+
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => {console.log(props); return <p>Number of exercises</p>}
+
+const Course = (props) => (
+  <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+)
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
