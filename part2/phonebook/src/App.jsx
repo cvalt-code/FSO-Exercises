1f7be80ceed89b199aee2bab1086ebde4573c44f
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-05 10:18:25 -0700

    exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,36 +1,46 @@
 import { useState } from 'react'
 
 const Person = ({ person }) => {
-  return <li>{person.name}</li>
+  return <li>{person.name} {person.number}</li>
 }
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' ,
+      number: '666-312-2133'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNoteChange = (event) => {    
   console.log(event.target.value)    
   setNewName(event.target.value)  }
 
+  const handleNumberChange = (event) => {    
+  console.log(event.target.value)    
+  setNewNumber(event.target.value)  }
+
   const addPerson = (event) => {
   event.preventDefault()
   const personObject = {
     name: newName,
+    number: newNumber,
   }
   const isRepeated = persons.some(person => person.name === newName);
   if (isRepeated) {
     alert(`${newName} is already added to the phonebook`)
     setNewName('');
+    setNewNumber('');
     
   }
   else {
   console.log(personObject);
   
   setPersons(persons.concat(personObject))
-  setNewName('')}
+  setNewName('')
+  setNewNumber('');}
 }
 
   return (
@@ -39,6 +49,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
