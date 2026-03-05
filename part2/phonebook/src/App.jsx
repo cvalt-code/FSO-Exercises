4dd0b9ba0c631e8f3b50977e02b5ce3c44ed0324
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-05 09:52:43 -0700

    exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return <li>{person.name}</li>
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNoteChange = (event) => {    
+  console.log(event.target.value)    
+  setNewName(event.target.value)  }
+
+  const addPerson = (event) => {
+  event.preventDefault()
+  const personObject = {
+    name: newName,
+  }
+  console.log(personObject);
+  
+  setPersons(persons.concat(personObject))
+  setNewName('')
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => (<Person key={i} person={person}/>))}
+    </div>
+  )
+}
+
+export default App
+
