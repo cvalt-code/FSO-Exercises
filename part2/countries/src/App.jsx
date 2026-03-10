ae67f64d00e851616f7cbdf56dee0265abfb6408
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-10 13:25:31 -0700

    exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,42 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
- 
+
+const CountryDisplay = ({name}) => {
+  const [countryData, setCountryData] = useState(null)
+
+  useEffect(() => {
+    axios
+  .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+  .then( response => {
+    setCountryData(response.data)
+  }
+  )
+
+  }, [name])
+  if (!countryData) {
+    return <div>Loading details for {name}...</div>
+  }
+  else {
+    console.log(countryData)
+  }
+
+  return(
+    <div>
+      <h1>{name}</h1>
+
+      <p>Capital: {countryData.capital}</p>
+      <p>Area: {countryData.area}</p>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(countryData.languages).map(l => (<li key={l}>{l}</li>))}
+      </ul>
+      <img src={countryData.flags.svg} width="300" height="200"/>
+    
+    </div>
+  )
+}
+
 const Country = (props) => {
   console.log(props.countryList)
   if (props.countryList.length > 10) {
@@ -9,6 +44,12 @@ const Country = (props) => {
       <div> Too many matches, specify another filter</div>
     )
   }
+  if (props.countryList.length === 1) {
+    return(
+      <CountryDisplay name={props.countryList[0].common}/>
+    )
+  }
+
   else {
     return(
       <ul>
@@ -22,6 +63,20 @@ const App = (props) => {
   const [country, setCountry] = useState('')
   const [countryInfo, setCountryInfo] = useState([])
   const [countriesFiltered, setCountriesFiltered] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+
+useEffect(() => {
+        console.log('effect run once, downloading all country data')
+        console.log('fetching countries...')
+        axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then(response => {
+        setAllCountries(response.data)
+          
+ 
+        })
+    }
+  , [])
 
   useEffect(() => {
     console.log('effect run, country is now', country)
@@ -29,14 +84,8 @@ const App = (props) => {
     // skip if currency is not defined
     if (country) {
       console.log('fetching countries...')
-      axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-        .then(response => {
-          const countries = response.data.map(c => c.name)
-          console.log(country)
-          setCountriesFiltered(countries.filter(c => c.common.toLowerCase().startsWith(country.toLowerCase())))
- 
-        })
+      const countries = allCountries.map(c => c.name)
+      setCountriesFiltered(countries.filter(c => c.common.toLowerCase().includes(country.toLowerCase())))
     }
   }, [country]) 
 
