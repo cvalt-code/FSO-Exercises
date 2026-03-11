611c6e31218fc5704bec78c98eab47d067d1b70c
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-11 13:06:29 -0700

    exercise 3.3

---
+++ part3/phonebook/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    personsFiltered = persons.find(person => person.id === id)
+      if (personsFiltered) {    
+        response.json(personsFiltered)  
+    } else {    
+        response.status(404).end()  }
+})
+
+app.get('/info', (request, response) => {
+    const personLength = persons.length
+    const currentDate = new Date();
+    const stringResponse = `<p>Phonebook has info for ${personLength} people</p>
+    <p>${currentDate.toString()}</p>
+    `;
+    
+  
+    response.send(stringResponse)
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
