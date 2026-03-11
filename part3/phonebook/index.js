61d6e2ec76ef31e5b0094bcc48b5ab88eac5bb01
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-11 13:09:24 -0700

    exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -37,6 +37,12 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()  }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id != id)
+  response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
     const personLength = persons.length
     const currentDate = new Date();
