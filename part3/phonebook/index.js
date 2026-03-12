e34a94578907e4a3c7913d5df9dcfc75012c50a7
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-12 13:08:27 -0700

    exercise 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan');
 
 app.use(express.json())
+app.use(morgan('tiny'));
 
 let persons = [
     { 
@@ -81,6 +83,12 @@ app.get('/info', (request, response) => {
     response.send(stringResponse)
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
