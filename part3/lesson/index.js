cbafd33d1518a05b624675e5b0ead563efbd3f0c
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-15 12:34:20 -0700

    exercise 3.13

--- part3/lesson/index.js
+++ part3/lesson/index.js
@@ -1,41 +1,27 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const morgan = require('morgan');
-const cors = require('cors')
-
-app.use(cors())
+// const cors = require('cors')
+const Note = require('./models/note')
+// app.use(cors())
 app.use(express.static('dist'))
 
 app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let notes = [  {    
-    id: "1",    
-    content: "HTML is easy",    
-    important: true  },  
-    {    
-    id: "2",    
-    content: "Browser can execute only JavaScript!!",    
-    important: false  },  
-    {    
-        id: "3",    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        important: true  }]
-
-
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-    if (note) {    
-        response.json(note)  
-    } else {    
-        response.status(404).end()  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -56,23 +42,20 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,39 +1,23 @@
 const express = require('express')
+
+require('dotenv').config()
+
 const app = express()
 const morgan = require('morgan');
 const cors = require('cors')
-
+const Person = require('./models/person')
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.post('/api/persons', (request, response) => {
