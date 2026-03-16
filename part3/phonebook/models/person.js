746c01673ae50ecc0ebfe0aa503ec8d043a55002
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-16 14:45:05 -0700

    exercise 3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -10,7 +10,15 @@ const personSchema = new mongoose.Schema({
   name: {    
     type: String,    
     minLength: 3},
-  number: String,
+  number: {    
+    type: String,    
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }},
 })
 
 personSchema.set('toJSON', {
