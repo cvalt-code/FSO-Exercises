3db016b7fe650405c2c2a93737c8e51314e309cb
cvalt-code <thisisnotforyou000@gmail.com>
2026-03-22 18:06:18 -0700

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,6 +3,13 @@ const dummy = (blogs) => {
 }
 
 
+const totalLikes = (blogs) => {
+  const listOfLikes = blogs.map((blog) => blog.likes)
+  const total = listOfLikes.reduce((partialSum, a) => partialSum + a, 0)
+  return blogs.length === 0 ? 0 : total;
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
